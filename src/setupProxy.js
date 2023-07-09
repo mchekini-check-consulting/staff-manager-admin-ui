65991eecd18abce32f06362042a8034034e92ec6
xlincw0w <khaled.khazem@yahoo.com>
2023-07-09 14:14:27 +0100

    urls update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:8080",
+      target: "http://check-consulting.net",
       changeOrigin: true,
     })
   );
