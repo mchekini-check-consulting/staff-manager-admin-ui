b9f03777766b931f875d8926bce79f1210d0fba9
Abdallah Hameur lain <abdallah.hameurlain@eadn.dz>
2023-07-10 20:51:27 +0100

    add clients list

---
+++ src/layouts/Clients/ClientList.js
@@ -0,0 +1,47 @@
+import { DataGrid } from "@mui/x-data-grid";
+import { fakerFR as faker } from "@faker-js/faker";
+import { useEffect, useState } from "react";
+
+const ClientList = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const rows = [];
+
+    for (let i = 1; i <= 50; i++) {
+      const row = {
+        id: i,
+        email: faker.internet.email(),
+        nom: faker.person.fullName(),
+        adresse: faker.location.city() + faker.location.street() + faker.location.buildingNumber(),
+        telephone: faker.phone.number(),
+        tva: faker.number.int({ min: 5, max: 20 }),
+      };
+
+      rows.push(row);
+    }
+
+    setData(rows);
+  }, []);
+
+  useEffect(() => {
+    console.log("data", data);
+  });
+
+  const columns = [
+    { field: "id", headerName: "Id", flex: 0.5 },
+    { field: "email", headerName: "Email", flex: 1 },
+    { field: "nom", headerName: "Nom", flex: 1 },
+    { field: "adresse", headerName: "Adresse", flex: 2 },
+    { field: "telephone", headerName: "N° Telephone", flex: 1 },
+    { field: "tva", headerName: "N° TVA", flex: 1 },
+  ];
+
+  return (
+    <div>
+      <DataGrid rows={data} columns={columns} />
+    </div>
+  );
+};
+
+export default ClientList;
--- src/layouts/Clients/index.js
+++ src/layouts/Clients/index.js
@@ -1,14 +1,20 @@
 import ContentLayout from "components/ContentLayout";
 import ContentNavbar from "components/ContentNavbar";
 import NewClient from "./NewClient";
+import ClientList from "./ClientList";
 import { Stack } from "@mui/material";
 
 function Clients() {
   return (
     <ContentLayout>
       <ContentNavbar />
-      <Stack alignItems={"flex-end"}>
-        <NewClient />
+      <Stack gap={2}>
+        <Stack alignItems={"flex-end"}>
+          <NewClient />
+        </Stack>
+        <Stack>
+          <ClientList />
+        </Stack>
       </Stack>
     </ContentLayout>
   );
