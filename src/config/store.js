33c05c908be0053b93264c24f742576b453a4977
sskarim85@gmail.com <sskarim85@gmail.com>
2023-07-23 19:22:24 +0100

    refetch data when new mission is add + combine firstname and lastname in one cell

--- src/config/store.js
+++ src/config/store.js
@@ -3,6 +3,7 @@ import { generalApi } from "services/general.api";
 import clientsReducer from "services/clients/client.slice";
 import { clientsApi } from "services/clients/client.api.slice";
 import { collaboratorApi } from "../services/collaborator/collaborator.api.slice";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
   reducer: {
@@ -17,3 +18,5 @@ export const store = configureStore({
       .concat(clientsApi.middleware)
       .concat(collaboratorApi.middleware),
 });
+// setupListeners(store.dispatch);
+// export default store;
