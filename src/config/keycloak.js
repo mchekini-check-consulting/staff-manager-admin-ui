ad39ba4eeb02b55eb62be20e24058a0c281be779
mahdi chekini <mchekini@checkconsulting.fr>
2023-08-04 02:10:18 +0200

    update keycloak url and hide selection in datagrid

--- src/config/keycloak.js
+++ src/config/keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-  url: "http://ci.check-consulting.net:10000/auth/",
+  url: "https://keycloak.check-consulting.net/auth/",
   realm: "staff-manager-admin",
   clientId: "staff-manager-admin-client",
 });
