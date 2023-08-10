227de9f9dd62ffead4aba2314d177e48ae60d6fc
Abdallah Hameur lain <abdallah.hameurlain@eadn.dz>
2023-08-10 10:40:52 +0100

    fix : prettier

--- src/layouts/Society/SocietyInformation.js
+++ src/layouts/Society/SocietyInformation.js
@@ -5,22 +5,22 @@ import { useDispatch } from "react-redux";
 import { togglePopup } from "services/society/society.slice";
 
 const SocietyInformation = () => {
-    const dispatch = useDispatch();
-    const handleOpen = () => {
-        dispatch(togglePopup());
-    };
+  const dispatch = useDispatch();
+  const handleOpen = () => {
+    dispatch(togglePopup());
+  };
 
-    return (
-        <div>
-            <NewSocietyPopup />
-            <Stack gap={2}>
-                <EnteteDatagrid
-                    enteteText={"Societe"}
-                    ctaButtonOnClick={handleOpen}
-                    ctaButtonText={"Ajouter les données de la société"}
-                />
-            </Stack>
-        </div>
-    );
+  return (
+    <div>
+      <NewSocietyPopup />
+      <Stack gap={2}>
+        <EnteteDatagrid
+          enteteText={"Societe"}
+          ctaButtonOnClick={handleOpen}
+          ctaButtonText={"Ajouter les données de la société"}
+        />
+      </Stack>
+    </div>
+  );
 };
 export default SocietyInformation;
